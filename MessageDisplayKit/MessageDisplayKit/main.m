bd9826a797ddd341cd37b9c1630af5063d96ed28
Jack <xhzengAIB@gmail.com>
2014-04-24 11:49:04 +0800

    Add Project init github reg.

---
+++ MessageDisplayKit/MessageDisplayKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MessageDisplayKit
+//
+//  Created by qtone-1 on 14-4-24.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
