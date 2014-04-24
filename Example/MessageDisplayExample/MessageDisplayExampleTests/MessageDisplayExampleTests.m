8dbe321c19c0c869502101c151a2fe5496efe1d3
Jack <xhzengAIB@gmail.com>
2014-04-24 12:15:35 +0800

    rename for project and kit name

--- Example/MessageDisplayExample/MessageDisplayExampleTests/MessageDisplayExampleTests.m
+++ Example/MessageDisplayExample/MessageDisplayExampleTests/MessageDisplayExampleTests.m
@@ -1,6 +1,6 @@
 //
-//  MessageDisplayKitTests.m
-//  MessageDisplayKitTests
+//  MessageDisplayExampleTests.m
+//  MessageDisplayExampleTests
 //
 //  Created by qtone-1 on 14-4-24.
 //  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
@@ -8,11 +8,11 @@
 
 #import <XCTest/XCTest.h>
 
-@interface MessageDisplayKitTests : XCTestCase
+@interface MessageDisplayExampleTests : XCTestCase
 
 @end
 
-@implementation MessageDisplayKitTests
+@implementation MessageDisplayExampleTests
 
 - (void)setUp
 {
