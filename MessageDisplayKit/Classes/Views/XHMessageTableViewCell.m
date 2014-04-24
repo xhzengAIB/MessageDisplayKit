01dbffed45cf9c487a764d1721975faccd4a70e2
Jack <xhzengAIB@gmail.com>
2014-04-24 16:43:18 +0800

    init cell class

---
+++ MessageDisplayKit/Classes/Views/XHMessageTableViewCell.m
@@ -0,0 +1,36 @@
+//
+//  XHMessageTableViewCell.m
+//  MessageDisplayExample
+//
+//  Created by qtone-1 on 14-4-24.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHMessageTableViewCell.h"
+
+@implementation XHMessageTableViewCell
+
+#pragma mark - Life cycle
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
