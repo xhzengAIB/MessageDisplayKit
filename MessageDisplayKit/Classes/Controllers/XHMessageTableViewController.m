c6afc241ce46d797ec482aa175512c53536db197
Aevit <Aevitx@gmail.com>
2014-04-28 16:10:31 +0800

    fix the send button's enable state after sended text for iOS7

--- MessageDisplayKit/Classes/Controllers/XHMessageTableViewController.m
+++ MessageDisplayKit/Classes/Controllers/XHMessageTableViewController.m
@@ -456,6 +456,14 @@
     DLog(@"text : %@", text);
     if ([self.delegate respondsToSelector:@selector(didSendText:fromSender:onDate:)]) {
         [self.delegate didSendText:text fromSender:self.messageSender onDate:[NSDate date]];
+        
+        if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
+            self.messageInputView.inputTextView.enablesReturnKeyAutomatically = NO;
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.messageInputView.inputTextView.enablesReturnKeyAutomatically = YES;
+                [self.messageInputView.inputTextView reloadInputViews];
+            });
+        }
     }
 }
 
