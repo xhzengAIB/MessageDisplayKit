9b84520f05c8e93a5ac11aa90afa9ce5f3a5d0e4
Jack <xhzengAIB@gmail.com>
2014-05-06 00:48:00 +0800

    Add more controller class due with display message

---
+++ MessageDisplayKit/Classes/Controllers/XHDisplayLocationViewController/XHDisplayLocationViewController.m
@@ -0,0 +1,40 @@
+//
+//  XHDisplayLocationViewController.m
+//  MessageDisplayExample
+//
+//  Created by qtone-1 on 14-5-6.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHDisplayLocationViewController.h"
+
+@interface XHDisplayLocationViewController ()
+
+@end
+
+@implementation XHDisplayLocationViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ MessageDisplayKit/Classes/Controllers/XHDisplayPhotoViewController/XHDisplayPhotoViewController.m
@@ -0,0 +1,40 @@
+//
+//  XHDisplayPhotoViewController.m
+//  MessageDisplayExample
+//
+//  Created by qtone-1 on 14-5-6.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHDisplayPhotoViewController.h"
+
+@interface XHDisplayPhotoViewController ()
+
+@end
+
+@implementation XHDisplayPhotoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
