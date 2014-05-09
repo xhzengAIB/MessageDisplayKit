d667570ffeb07ba01bb27fbb8cdb887e6a981925
Jack <xhzengAIB@gmail.com>
2014-05-10 01:53:49 +0800

    Fix the error and update v0.1.8

--- MessageDisplayKit/Classes/Controllers/XHProfileTableViewController/XHProfileTableViewController.h
+++ MessageDisplayKit/Classes/Controllers/XHProfileTableViewController/XHProfileTableViewController.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
 //
 
-#import <MessageDisplayFramework/MessageDisplayFramework.h>
+#import "XHBaseTableViewController.h"
 
 @interface XHProfileTableViewController : XHBaseTableViewController
 
