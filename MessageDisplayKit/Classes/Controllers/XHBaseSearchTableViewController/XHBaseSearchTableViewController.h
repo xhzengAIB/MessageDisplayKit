183393931dca6f3500ecc92e391081eca59da4d8
Jack <xhzengAIB@gmail.com>
2014-05-22 01:45:01 +0000

    init search tableview controller.

--- MessageDisplayKit/Classes/Controllers/XHBaseSearchTableViewController/XHBaseSearchTableViewController.h
+++ MessageDisplayKit/Classes/Controllers/XHBaseSearchTableViewController/XHBaseSearchTableViewController.h
@@ -1,15 +1,15 @@
 //
-//  XHContact.h
+//  XHBaseSearchTableViewController.h
 //  MessageDisplayExample
 //
 //  Created by 曾 宪华 on 14-5-22.
 //  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "XHBaseTableViewController.h"
 
-@interface XHContact : NSObject
+@interface XHBaseSearchTableViewController : XHBaseTableViewController
 
-@property (nonatomic, copy) NSString *contactName;
+@property (nonatomic, strong) NSMutableArray *filteredDataSource;
 
 @end
