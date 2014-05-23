a740dc5aa99ec4b07d423da309ed363dab9759b6
Jack <xhzengAIB@gmail.com>
2014-05-23 01:14:35 +0000

    Delete sleep for album loadDataSource.

--- MessageDisplayKit/Classes/Controllers/XHDiscoverTableViewController/XHAlbumTableViewController/XHAlbumTableViewController.m
+++ MessageDisplayKit/Classes/Controllers/XHDiscoverTableViewController/XHAlbumTableViewController/XHAlbumTableViewController.m
@@ -48,7 +48,6 @@
     WEAKSELF
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSMutableArray *dataSource = [[XHStoreManager shareStoreManager] getAlbumConfigureArray];
-        sleep(3);
         
         dispatch_async(dispatch_get_main_queue(), ^{
             [weakSelf.albumHeaderContainerViewPathCover stopRefresh];
