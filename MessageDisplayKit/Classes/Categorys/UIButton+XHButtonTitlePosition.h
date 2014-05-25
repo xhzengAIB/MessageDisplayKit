83c840600928e6e7c7f6e7a1e937ef896fb29697
Jack <xhzengAIB@gmail.com>
2014-05-25 03:30:08 +0000

    Add shake function.
    fix  https://github.com/xhzengAIB/MessageDisplayKit/issues/94

---
+++ MessageDisplayKit/Classes/Categorys/UIButton+XHButtonTitlePosition.h
@@ -0,0 +1,19 @@
+//
+//  UIButton+XHButtonTitlePosition.h
+//  MessageDisplayExample
+//
+//  Created by 曾 宪华 on 14-5-25.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, XHButtonTitlePostionType) {
+    kXHTitleBottomType = 0,
+};
+
+@interface UIButton (XHButtonTitlePosition)
+
+- (void)setTitlePositionWithType:(XHButtonTitlePostionType)type;
+
+@end
