89db2bebb592013fce2e565659adc7c4168276da
Jack <xhzengAIB@gmail.com>
2014-05-28 01:41:41 +0000

    setup the scanning tip logic.
    fix  https://github.com/xhzengAIB/MessageDisplayKit/issues/108

--- MessageDisplayKit/Classes/Views/ScanningView/XHScanningView.m
+++ MessageDisplayKit/Classes/Views/ScanningView/XHScanningView.m
@@ -8,6 +8,13 @@
 
 #import "XHScanningView.h"
 
+#define kXHQRCodeTipString @"将二维码/条码放入框内，即可自动扫描"
+#define kXHBookTipString @"将书、CD、电影海报放入框内，即可自动扫描"
+#define kXHStreetTipString @"扫一下周围环境，讯在附近街景"
+#define kXHWordTipString @"将英文单词放入框内"
+
+#define kXHQRCodeRectPaddingX 55
+
 typedef void(^TransformScanningAnimationBlock)(void);
 
 @interface XHScanningView ()
@@ -17,6 +24,10 @@ typedef void(^TransformScanningAnimationBlock)(void);
 @property (nonatomic, strong) UIImageView *scanningImageView;
 
 @property (nonatomic, assign) CGRect clearRect;
+
+@property (nonatomic, strong) UILabel *QRCodeTipLabel;
+
+@property (nonatomic, strong) UIButton *myQRCodeButton;
 
 @end
 
@@ -48,6 +59,31 @@ typedef void(^TransformScanningAnimationBlock)(void);
     return _scanningImageView;
 }
 
+- (UILabel *)QRCodeTipLabel {
+    if (!_QRCodeTipLabel) {
+        _QRCodeTipLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(self.clearRect) + 30, CGRectGetWidth(self.bounds) - 20, 20)];
+        _QRCodeTipLabel.text = kXHQRCodeTipString;
+        _QRCodeTipLabel.numberOfLines = 0;
+        _QRCodeTipLabel.textColor = [UIColor whiteColor];
+        _QRCodeTipLabel.backgroundColor = [UIColor clearColor];
+        _QRCodeTipLabel.textAlignment = NSTextAlignmentCenter;
+        _QRCodeTipLabel.font = [UIFont systemFontOfSize:12];
+    }
+    return _QRCodeTipLabel;
+}
+
+- (UIButton *)myQRCodeButton {
+    if (!_myQRCodeButton) {
+        _myQRCodeButton = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_QRCodeTipLabel.frame) + 30, 80, 20)];
+        _myQRCodeButton.center = CGPointMake(CGRectGetWidth(self.bounds) / 2.0, _myQRCodeButton.center.y);
+        [_myQRCodeButton setTitle:@"我的二维码" forState:UIControlStateNormal];
+        [_myQRCodeButton setTitleColor:[UIColor colorWithRed:0.275 green:0.491 blue:1.000 alpha:1.000] forState:UIControlStateNormal];
+        _myQRCodeButton.backgroundColor = [UIColor clearColor];
+        _myQRCodeButton.titleLabel.font = [UIFont systemFontOfSize:14];
+    }
+    return _myQRCodeButton;
+}
+
 #pragma mark - Public Api
 
 - (void)transformScanningTypeWithStyle:(XHScanningStyle)style {
@@ -66,7 +102,13 @@ typedef void(^TransformScanningAnimationBlock)(void);
     if (self) {
         // Initialization code
         self.backgroundColor = [UIColor colorWithWhite:0.000 alpha:0.500];
+        
+        self.clearRect = CGRectMake(kXHQRCodeRectPaddingX, 30, CGRectGetWidth(frame) - kXHQRCodeRectPaddingX * 2, CGRectGetWidth(frame) - kXHQRCodeRectPaddingX * 2);
+        
         [self addSubview:self.scanningImageView];
+        [self addSubview:self.QRCodeTipLabel];
+        [self addSubview:self.myQRCodeButton];
+        
         [self scanning];
     }
     return self;
@@ -86,20 +128,37 @@ typedef void(^TransformScanningAnimationBlock)(void);
     CGRect clearRect;
     CGFloat paddingX;
     
+    CGFloat tipLabelPadding;
+    
     self.scanningImageView.hidden = YES;
+    self.myQRCodeButton.hidden = YES;
     switch (self.scanningStyle) {
         case kXHQRCodeStyle: {
-            paddingX = 55;
-            clearRect = CGRectMake(paddingX, 30, CGRectGetWidth(rect) - paddingX * 2, CGRectGetWidth(rect) - paddingX * 2);
+            tipLabelPadding = 30;
+            self.QRCodeTipLabel.text = kXHQRCodeTipString;
+            
+            self.myQRCodeButton.hidden = NO;
             self.scanningImageView .hidden = NO;
+            paddingX = kXHQRCodeRectPaddingX;
+            clearRect = CGRectMake(paddingX, 30, CGRectGetWidth(rect) - paddingX * 2, CGRectGetWidth(rect) - paddingX * 2);
             break;
         }
         case kXHStreetStyle:
         case kXHBookStyle:
+            tipLabelPadding = 20;
+            if (self.scanningStyle == kXHStreetStyle) {
+                self.QRCodeTipLabel.text = kXHStreetTipString;
+            } else {
+                self.QRCodeTipLabel.text = kXHBookTipString;
+            }
+            
             paddingX = 20;
             clearRect = CGRectMake(paddingX, 20, CGRectGetWidth(rect) - paddingX * 2, CGRectGetWidth(rect) - paddingX * 2);
             break;
         case kXHWordStyle:
+            tipLabelPadding = 25;
+            self.QRCodeTipLabel.text = kXHWordTipString;
+            
             paddingX = 50;
             clearRect = CGRectMake(paddingX, 100, CGRectGetWidth(rect) - paddingX * 2, 50);
             break;
@@ -108,6 +167,10 @@ typedef void(^TransformScanningAnimationBlock)(void);
     }
     
     self.clearRect = clearRect;
+    
+    CGRect QRCodeTipLabelFrame = self.QRCodeTipLabel.frame;
+    QRCodeTipLabelFrame.origin.y = CGRectGetMaxY(self.clearRect) + tipLabelPadding;
+    self.QRCodeTipLabel.frame = QRCodeTipLabelFrame;
     
     CGContextClearRect(context, clearRect);
     
