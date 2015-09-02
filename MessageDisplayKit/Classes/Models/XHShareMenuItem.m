746923cfaf113a17b42bd2e1cb25cddba55aa941
Jack <xhzengAIB@gmail.com>
2015-09-03 01:19:23 +0800

    定制MenuItem

--- MessageDisplayKit/Classes/Models/XHShareMenuItem.m
+++ MessageDisplayKit/Classes/Models/XHShareMenuItem.m
@@ -12,10 +12,19 @@
 
 - (instancetype)initWithNormalIconImage:(UIImage *)normalIconImage
                                   title:(NSString *)title {
+    return [self initWithNormalIconImage:normalIconImage title:title titleColor:nil titleFont:nil];
+}
+
+- (instancetype)initWithNormalIconImage:(UIImage *)normalIconImage
+                                  title:(NSString *)title
+                             titleColor:(UIColor *)titleColor
+                              titleFont:(UIFont *)titleFont {
     self = [super init];
     if (self) {
         self.normalIconImage = normalIconImage;
         self.title = title;
+        self.titleColor = titleColor;
+        self.titleFont = titleFont;
     }
     return self;
 }
