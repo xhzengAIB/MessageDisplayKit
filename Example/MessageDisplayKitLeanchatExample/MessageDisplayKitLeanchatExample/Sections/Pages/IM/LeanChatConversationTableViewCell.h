5e50267d2d9aaa6cc408b77a4a1863a1f304e56c
Jack <xhzengAIB@gmail.com>
2016-03-15 14:05:42 +0800

    修改描述

--- Example/MessageDisplayKitLeanchatExample/MessageDisplayKitLeanchatExample/Sections/Pages/IM/LeanChatConversationTableViewCell.h
+++ Example/MessageDisplayKitLeanchatExample/MessageDisplayKitLeanchatExample/Sections/Pages/IM/LeanChatConversationTableViewCell.h
@@ -3,7 +3,7 @@
 //  MessageDisplayKitLeanchatExample
 //
 //  Created by lzw on 15/4/17.
-//  Copyright (c) 2015年 iOS软件开发工程师 曾宪华 热衷于简洁的UI QQ:543413507 http://www.pailixiu.com/blog   http://www.pailixiu.com/Jack/personal. All rights reserved.
+//  Copyright (c) 2015年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- MessageDisplayKit/Classes/Controllers/XHMessageTableViewController/XHDisplayEmotionViewController/XHDisplayEmotionViewController.m
+++ MessageDisplayKit/Classes/Controllers/XHMessageTableViewController/XHDisplayEmotionViewController/XHDisplayEmotionViewController.m
@@ -3,7 +3,7 @@
 //  MessageDisplayExample
 //
 //  Created by HUAJIE-1 on 14-5-6.
-//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//  Copyright (c) 2014年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
 
 #import "XHDisplayEmotionViewController.h"
